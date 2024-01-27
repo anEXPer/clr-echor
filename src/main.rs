28911976fd069d5466334bc30832e2c42ae37082
Jesse Alord <inviolate.mind@gmail.com>
2024-01-26 16:39:20 -0800

    Refactor to use clap parser with derive
    - Instead of that crappy magic arithmatic,
      we've got real flag parsing.
    - This isn't... _strictly_ a refactor.
      A bunch of additional utility (usage, etc) comes along free.
      We might decorate in coverage for it if we end up wanting to.
    - We'll obviously want to get rid of the "ignore flags" case too.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 #![warn(clippy::all, clippy::pedantic)]
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+/// Rust version `echo` written as practice
+struct Args {
+    ///Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    ///Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+
+    ///Backwards compatability for ignore flags test
+    #[arg(long("flag"))]
+    ignore_flags: bool,
+}
 
 fn main() {
-    let mut args: Vec<String> = std::env::args()
-        .filter(|arg| !arg.starts_with('-'))
-        .collect();
+    let args = Args::parse();
 
-    args.remove(0);
-
-    if std::env::args().len() > args.len() + 1 {
-        print!("{}", args.join(" "));
+    if args.omit_newline {
+        print!("{}", args.text.join(" "));
     } else {
-        println!("{}", args.join(" "));
+        println!("{}", args.text.join(" "));
     }
 }
